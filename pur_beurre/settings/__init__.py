172a2df19a87e0865e919ea6e23395ce1fddca02
Leonard Colin <leooo.colin@gmail.com>
2021-03-10 19:27:58 +0100

    Update settings

--- pur_beurre/settings/__init__.py
+++ pur_beurre/settings/__init__.py
@@ -24,7 +24,8 @@ environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 # BASE_DIR = Path(__file__).resolve().parent.parent
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
@@ -152,5 +153,3 @@ MESSAGE_TAGS = {
     messages.WARNING: 'alert-warning',
     messages.ERROR: 'alert-danger',
 }
-
-django_heroku.settings(locals())
